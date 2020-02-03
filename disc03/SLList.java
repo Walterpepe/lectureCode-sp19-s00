6d0c09615b50bd7ca7d6196257ced6b58d2195eb
liyu <1271946196@qq.com>
2020-02-03 17:12:42 +0800

    disc03 SLList, completed version

--- disc03/SLList.java
+++ disc03/SLList.java
@@ -1,3 +1,5 @@
+import java.util.Formatter;
+
 public class SLList {
     private class IntNode {
         public int item;
@@ -9,24 +11,30 @@ public class SLList {
         }
     }
 
-    private IntNode first;
+    private IntNode sentinel;
+    private int size;
 
     public SLList() {
-        first = new IntNode(-1, null);
+        sentinel = new IntNode(-1, null);
+        size = 0;
     }
 
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
+        size++;
     }
 
+    // solution to problem 1.1
     public void insert(int item, int position) {
 
-        IntNode ptr = first;
+        IntNode ptr = sentinel;
         int index = 0;
-        while(true){
-            if (position == index || ptr.next == null){
+        while (true) {
+            if (position == index || ptr.next == null) {
                 IntNode insertNode = new IntNode(item, ptr.next);
                 ptr.next = insertNode;
+                size++;
+                break;
             }
             ptr = ptr.next;
             index++;
@@ -34,4 +42,155 @@ public class SLList {
 
     }
 
+    // problem 1.2
+    public void reverse() {
+
+        if (size == 0) {
+            return;
+        }
+
+        IntNode ptr0 = null;
+        IntNode ptr1 = sentinel.next;
+        IntNode ptr2 = sentinel.next.next;
+
+        while (ptr2 != null) {
+
+            IntNode ptr3 = ptr2.next;
+
+            ptr2.next = ptr1;
+            ptr1.next = ptr0;
+
+            ptr0 = ptr1;
+            ptr1 = ptr2;
+            ptr2 = ptr3;
+        }
+
+        sentinel.next = ptr1;
+
+    }
+
+    // problem 1.3 递归版本
+    // TODO
+    public void reverseRecursion() {
+
+    }
+
+    // 从IntList拷贝，做了一些修改。
+
+    /**
+     * DO NOT MODIFY ANYTHING BELOW THIS LINE! Many of the concepts below here
+     * will be introduced later in the course or feature some form of advanced
+     * trickery which we implemented to help make your life a little easier for
+     * the lab.
+     */
+
+    @Override
+    public int hashCode() {
+        return size;
+    }
+
+    /**
+     * Returns a new IntList containing the ints in ARGS. You are not
+     * expected to read or understand this method.
+     */
+    public static SLList of(Integer... args) {
+        SLList result = new SLList();
+
+        int k;
+        for (k = args.length - 1; k >= 0; k--) {
+            result.addFirst(args[k]);
+        }
+        return result;
+    }
+
+    /**
+     * Returns true iff X is an IntList containing the same sequence of ints
+     * as THIS. Cannot handle IntLists with cycles. You are not expected to
+     * read or understand this method.
+     */
+    public boolean equals(Object x) {
+        if (!(x instanceof SLList)) {
+            return false;
+        }
+        SLList SL = (SLList) x;
+        IntNode L = SL.sentinel.next;
+        IntNode p;
+
+        for (p = this.sentinel.next; p != null && L != null; p = p.next, L = L.next) {
+            if (p.item != L.item) {
+                return false;
+            }
+        }
+        if (p != null || L != null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * If a cycle exists in the IntList, this method
+     * returns an integer equal to the item number of the location where the
+     * cycle is detected.
+     * <p>
+     * If there is no cycle, the number 0 is returned instead. This is a
+     * utility method for lab2. You are not expected to read, understand, or
+     * even use this method. The point of this method is so that if you convert
+     * an IntList into a String and that IntList has a loop, your computer
+     * doesn't get stuck in an infinite loop.
+     */
+
+    private int detectCycles(SLList A) {
+        IntNode tortoise = A.sentinel.next;
+        IntNode hare = A.sentinel.next;
+
+        if (A == null) {
+            return 0;
+        }
+
+        int cnt = 0;
+
+
+        while (true) {
+            cnt++;
+            if (hare.next != null) {
+                hare = hare.next.next;
+            } else {
+                return 0;
+            }
+
+            tortoise = tortoise.next;
+
+            if (tortoise == null || hare == null) {
+                return 0;
+            }
+
+            if (hare == tortoise) {
+                return cnt;
+            }
+        }
+    }
+
+    @Override
+    /** Outputs the IntList as a String. You are not expected to read
+     * or understand this method. */
+    public String toString() {
+        Formatter out = new Formatter();
+        String sep;
+        sep = "(";
+        int cycleLocation = detectCycles(this);
+        int cnt = 0;
+
+        for (IntNode p = this.sentinel.next; p != null; p = p.next) {
+            out.format("%s%d", sep, p.item);
+            sep = ", ";
+
+            cnt++;
+            if ((cnt > cycleLocation) && (cycleLocation > 0)) {
+                out.format("... (cycle exists) ...");
+                break;
+            }
+        }
+        out.format(")");
+        return out.toString();
+    }
 }
--- disc03/SLListTest.java
+++ disc03/SLListTest.java
@@ -1,2 +1,59 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class SLListTest {
+
+    @Test
+    public void testOf() {
+        SLList s1 = new SLList();
+        s1.addFirst(2);
+        s1.addFirst(6);
+        s1.addFirst(5);
+
+        SLList x = SLList.of(5, 6, 2);
+        assertEquals(s1, x);
+    }
+
+    @Test
+    public void testInsert() {
+        // Case 1
+        SLList s1 = new SLList();
+        s1.addFirst(2);
+        s1.addFirst(6);
+        s1.addFirst(5);
+
+        s1.insert(10, 1);
+        assertEquals(s1, SLList.of(5, 10, 6, 2));
+
+        // Case 2
+        SLList s2 = new SLList();
+        s2.addFirst(2);
+        s2.addFirst(6);
+        s2.addFirst(5);
+
+        s2.insert(10, 7);
+        assertEquals(s2, SLList.of(5, 6, 2, 10));
+
+    }
+
+
+    @Test
+    public void TestReverse() {
+        // Case 1
+        SLList s1 = SLList.of(5, 6, 2);
+        s1.reverse();
+        assertEquals(s1, SLList.of(2, 6, 5));
+
+        // Case 2
+        SLList s2 = SLList.of(5);
+        s2.reverse();
+        assertEquals(s2, SLList.of(5));
+
+        // Case 3
+        SLList s3 = SLList.of();
+        s3.reverse();
+        assertEquals(s3, SLList.of());
+    }
+
 }
